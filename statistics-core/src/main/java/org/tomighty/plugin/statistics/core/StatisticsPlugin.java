09709675a8f09109482d4a48fdcef8781131d4da
Dominik Obermaier <dominik.obermaier@gmail.com>
2012-05-13 22:58:58 +0200

    removed statistics core menu hook

--- statistics-core/src/main/java/org/tomighty/plugin/statistics/core/StatisticsPlugin.java
+++ statistics-core/src/main/java/org/tomighty/plugin/statistics/core/StatisticsPlugin.java
@@ -29,9 +29,6 @@ import org.tomighty.plugin.statistics.core.subscriber.TimerStartedSubscriber;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class StatisticsPlugin extends DefaultPlugin {
 
@@ -64,17 +61,5 @@ public class StatisticsPlugin extends DefaultPlugin {
 
 
         log.info("Statistics Plugin initialized");
-    }
-
-    @Override
-    public MenuItem getMenuItem() {
-        MenuItem statistics = new MenuItem("Statistics");
-        statistics.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(final ActionEvent e) {
-                log.info("Statistics clicked");
-            }
-        });
-        return statistics;
     }
 }
